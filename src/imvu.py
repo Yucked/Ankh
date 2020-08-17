0ef9ec27705bcd0172479bb89f398d987e0564f7
Yucked <yucked@protonmail.com>
2020-08-16 21:19:09 -0400

    initial profile fetching done

---
+++ src/imvu.py
@@ -0,0 +1,35 @@
+import requests
+import logging
+
+def get_profile(username):
+    cid = get_cid(username)
+    response = requests.get(f'http://www.imvu.com/api/avatarcard.php?cid={cid}')
+    
+    ## Check for status_code
+
+    return response.text
+
+def get_cid(username):
+    request_body = f"""<?xml version="1.0" encoding="UTF-8"?>
+<methodCall>
+<methodName>gateway.getUserIdForAvatarName</methodName>
+<params>
+    <param>
+        <value>
+            <string>{username}</string>
+        </value>
+    </param>
+</params>
+</methodCall>"""
+    response = requests.post(
+        'http://secure.imvu.com//catalog/skudb/gateway.php',
+        data = request_body,
+        headers = {'Content-Type': 'application/xml'})
+
+    return response.text[106:response.text.index('</int>')]
+
+def get_privates(username):
+    return username
+
+def get_room_history(username):
+    return username
