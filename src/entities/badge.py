a06788179e9b096e49531aaec6901bb9e4556b92
Yucked <yucked@protonmail.com>
2020-08-16 22:17:16 -0400

    badge props

--- src/entities/badge.py
+++ src/entities/badge.py
@@ -5,3 +5,10 @@ class Badge:
         # badge = json.loads(badge_json)
         self.name = badge['name']
         self.image = badge['image_url']
+        self.id = badge['badgeid']
+        self.badge_type = badge['badge_type']
+
+        self.creator_id = badge['creator_id']
+        self.badge_index = badge['creator_badge_index']
+
+        self.is_autogrant = badge['allow_autogrant']
