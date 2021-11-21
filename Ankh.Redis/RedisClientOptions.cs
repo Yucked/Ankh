9796a754d05610b676f1d30b933bb5fa7165ee1c
Yucked <Yucked@pm.me>
2021-11-21 14:53:35 -0500

    Client connection and options

---
+++ Ankh.Redis/RedisClientOptions.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Ankh.Redis;
+
+/// <summary>
+/// 
+/// </summary>
+public readonly record struct RedisClientOptions {
+    /// <summary>
+    /// 
+    /// </summary>
+    public int DatabaseIndex { get; init; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool TypeNameAsKeyPrefix { get; init; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Hostname { get; init; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public JsonSerializerOptions JsonOptions { get; init; }
+        = new() {
+            DictionaryKeyPolicy = JsonNamingPolicy.CamelCase,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            DefaultIgnoreCondition = JsonIgnoreCondition.Never,
+            WriteIndented = true,
+            PropertyNameCaseInsensitive = true
+        };
+}
