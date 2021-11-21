4cfc082ea4f3669e7f7374900025eff8386ed455
Yucked <Yucked@pm.me>
2021-11-21 14:53:02 -0500

    Added IRedisEntity

---
+++ Ankh.Redis/Interfaces/IRedisEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace Ankh.Redis.Interfaces;
+
+/// <summary>
+/// 
+/// </summary>
+public interface IRedisEntity {
+    /// <summary>
+    /// 
+    /// </summary>
+    string Id { get; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    DateOnly AddedOn { get; }
+}
