621b9d84e68e1b99810a3db55a0c836a219d0af0
Yucked <Yucked@pm.me>
2021-11-30 20:12:19 -0500

    finished roomhandler

--- src/Handlers/RoomHandler.cs
+++ src/Handlers/RoomHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text;
 using System.Text.Json;
 using Ankh.Data;
+using Ankh.Redis;
 
 namespace Ankh.Handlers;
 
@@ -10,6 +11,16 @@ public class RoomHandler {
 
     private static readonly ReadOnlyMemory<byte> ScriptEnd
         = new byte[] {60, 47, 115, 99, 114, 105, 112, 116, 62};
+
+    private readonly RedisClientManager _redisClientManager;
+    private readonly ILogger<UserHandler> _logger;
+    private readonly HttpClient _httpClient;
+
+    public RoomHandler(RedisClientManager redisClientManager, ILogger<UserHandler> logger, HttpClient httpClient) {
+        _redisClientManager = redisClientManager;
+        _logger = logger;
+        _httpClient = httpClient;
+    }
 
     public static RoomData ToRoom(Span<byte> data, string roomUrl) {
         var slice = data[(data.IndexOf(RoomInfoVar.Span) + RoomInfoVar.Length)..];
@@ -27,25 +38,21 @@ public class RoomHandler {
             participants[name!] = DateTimeOffset.UtcNow;
         }
 
-        return default;
-        /*
-        return new RoomData(document.Get<string>("roomInstanceId")) {
-            Id = document.GetProperty("roomInstanceId").GetString(),
-            Owner = document.GetProperty("owner").GetString(),
-            Name = document.GetProperty("room_name").GetString()!.Decode(),
-            Description = document.GetProperty("desc").GetString()!.Decode(),
-            Capacity = int.Parse(document.GetProperty("max").GetString()!),
-            Occupancy = int.Parse(document.GetProperty("count").GetString()!),
-            Image = document.GetProperty("img").GetString(),
-            IsApOnly = string.IsNullOrWhiteSpace(document.GetProperty("ap").GetString()),
-            IsVipOnly = string.IsNullOrWhiteSpace(document.GetProperty("vip").GetString()),
-            ApNameOnly = document.GetProperty("show_ap_name_only").GetBoolean(),
-            Ratings = document.GetProperty("whitelist_rating").GetInt32(),
-            Url = roomUrl.Decode(),
-            History = participants,
-            AddedOn = DateOnly.FromDateTime(DateTime.Now)
-        };
-        */
+        return new RoomData(
+            document.Get<string>("roomInstanceId"),
+            DateOnly.FromDateTime(DateTime.Now),
+            document.Get<string>("room_name", JsonType.Decode),
+            string.IsNullOrWhiteSpace(document.GetProperty("ap").GetString()),
+            string.IsNullOrWhiteSpace(document.GetProperty("vip").GetString()),
+            document.Get<string>("owner"),
+            document.Get<string>("desc", JsonType.Decode),
+            document.Get<int>("max", JsonType.StrToInt),
+            document.Get<int>("count", JsonType.StrToInt),
+            document.Get<string>("img"),
+            document.GetProperty("whitelist_rating").GetInt32(),
+            roomUrl.Decode(),
+            document.GetProperty("show_ap_name_only").GetBoolean(),
+            participants);
     }
 
     public static RoomData Update(RoomData before, RoomData after) {
@@ -55,5 +62,27 @@ public class RoomHandler {
         }
 
         return updated;
+    }
+
+    public async Task CacheRoomAsync(string url) {
+        using var requestMessage = new HttpRequestMessage {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri(url)
+        };
+        using var responseMessage = await _httpClient.SendAsync(requestMessage);
+        if (!responseMessage.IsSuccessStatusCode) {
+            _logger.LogWarning("{ReasonPhrase}", responseMessage.ReasonPhrase);
+            return;
+        }
+
+        using var content = responseMessage.Content;
+        var byteData = await content.ReadAsByteArrayAsync();
+        try {
+            var room = ToRoom(byteData, url);
+            await _redisClientManager.For<RoomData>().AddAsync(room);
+        }
+        catch (Exception exception) {
+            _logger.LogCritical("{Message} {exception}", exception.Message, exception);
+        }
     }
 }
