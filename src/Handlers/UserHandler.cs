69b853ef8ffc372ccfb079e5d25d580cc8c6bb24
Yucked <Yucked@pm.me>
2021-11-30 20:12:27 -0500

    finished userhandler

--- src/Handlers/UserHandler.cs
+++ src/Handlers/UserHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Text;
 using System.Text.Json;
 using Ankh.Data;
 using Ankh.Redis;
@@ -7,9 +8,16 @@ namespace Ankh.Handlers;
 
 public sealed class UserHandler {
     private readonly RedisClientManager _redisClientManager;
+    private readonly ILogger<UserHandler> _logger;
+    private readonly HttpClient _httpClient;
 
-    public UserHandler(RedisClientManager redisClientManager) {
+    private static readonly ReadOnlyMemory<byte> EndSegment
+        = new byte[] {60, 47, 105, 110, 116, 62};
+
+    public UserHandler(RedisClientManager redisClientManager, ILogger<UserHandler> logger, HttpClient httpClient) {
         _redisClientManager = redisClientManager;
+        _logger = logger;
+        _httpClient = httpClient;
     }
 
     public async Task JsonToUserAsync(Stream stream) {
@@ -21,33 +29,33 @@ public sealed class UserHandler {
                 Country = rootElement.Get<string>("location"),
                 CountryCode = rootElement.Get<int>("country_code"),
                 State = rootElement.Get<string>("location_state"),
-                IsFlagVisible = rootElement.Get<bool>("show_flag_av", JsonGetOptions.IntToBool),
-                IsFlagIconVisible = rootElement.Get<bool>("show_flag_icon", JsonGetOptions.IntToBool)
+                IsFlagVisible = rootElement.Get<bool>("show_flag_av", JsonType.IntToBool),
+                IsFlagIconVisible = rootElement.Get<bool>("show_flag_icon", JsonType.IntToBool)
             };
         }
 
         Avatar GetAvatarPicture() {
             return new Avatar {
                 Url = rootElement.Get<string>("avpic_url"),
-                IsDefault = rootElement.Get<bool>("avpic_default", JsonGetOptions.IntToBool)
+                IsDefault = rootElement.Get<bool>("avpic_default", JsonType.IntToBool)
             };
         }
 
         Moderator GetModeratorData() {
             return new Moderator {
                 WelcomeScore = rootElement.Get<int>("welcome_moderator_score"),
-                IsModerator = rootElement.Get<bool>("is_welcome_moderator", JsonGetOptions.IntToBool)
+                IsModerator = rootElement.Get<bool>("is_welcome_moderator", JsonType.IntToBool)
             };
         }
 
         Misc GetMiscUserData() {
             return new Misc {
                 IsBuddy = rootElement.Get<bool>("is_buddy"),
-                IsFriend = rootElement.Get<bool>("is_friend", JsonGetOptions.IntToBool),
+                IsFriend = rootElement.Get<bool>("is_friend", JsonType.IntToBool),
                 IsQualityAssurance = rootElement.Get<bool>("is_qa"),
                 ShowBlock = rootElement.Get<bool>("show_block"),
-                ShowMessage = rootElement.Get<bool>("show_message", JsonGetOptions.IntToBool),
-                IsCreator = rootElement.Get<bool>("is_creator", JsonGetOptions.IntToBool),
+                ShowMessage = rootElement.Get<bool>("show_message", JsonType.IntToBool),
+                IsCreator = rootElement.Get<bool>("is_creator", JsonType.IntToBool),
                 ImvuLevel = rootElement.Get<int>("imvu_level"),
                 WallpaperId = rootElement.Get<int>("wallpaper_id"),
             };
@@ -78,8 +86,8 @@ public sealed class UserHandler {
             Id = rootElement.GetProperty("cid").GetInt32().ToString(),
             Username = rootElement.Get<string>("avname"),
             Homepage = rootElement.Get<string>("url"),
-            RegisteredOn = rootElement.Get<DateOnly>("registered", JsonGetOptions.ParseDate),
-            LastLogon = rootElement.Get<DateOnly>("last_login", JsonGetOptions.ParseDate),
+            RegisteredOn = rootElement.Get<DateOnly>("registered", JsonType.ParseDate),
+            LastLogon = rootElement.Get<DateOnly>("last_login", JsonType.ParseDate),
             Gender = rootElement.Get<string>("gender"),
             Tagline = rootElement.Get<string>("tagline"),
             IsOnline = rootElement.Get<bool>("online"),
@@ -93,7 +101,8 @@ public sealed class UserHandler {
             Interests = GetInterest(),
             Age = GetAge(),
             Usernames = new HashSet<string>(),
-            AddedOn = DateOnly.FromDateTime(DateTime.Now)
+            AddedOn = DateOnly.FromDateTime(DateTime.Now),
+            PublicRooms = default
         };
 
         var badges = new Badges {
@@ -119,7 +128,23 @@ public sealed class UserHandler {
             });
         }
 
+        var roomIds = new List<string>();
+        if (rootElement.TryGetProperty("public_rooms", out var roomsElement)) {
+            await Parallel.ForEachAsync(roomsElement.EnumerateObject(), async (x, token) => {
+                var roomData = new PublicRoomData(
+                    x.Get<string>("room_instance_id"),
+                    Extensions.Today(),
+                    x.Get<string>("name", JsonType.Decode),
+                    x.Get<bool>("is_ap", JsonType.IntToBool),
+                    x.Get<bool>("is_vip", JsonType.IntToBool));
+
+                roomIds.Add(roomData.Id);
+                await _redisClientManager.For<RoomData>().AddAsync(roomData as RoomData);
+            });
+        }
+
         userData.Badges = badges;
+        userData.PublicRooms = roomIds;
         await _redisClientManager.For<UserData>().AddAsync(userData);
     }
 
@@ -128,12 +153,12 @@ public sealed class UserHandler {
             .Select(x => new BadgeData {
                 Id = x.Get<string>("badgeid"),
                 Name = x.Get<string>("name"),
-                IsAutogranted = x.Get<bool>("allow_autogrant", JsonGetOptions.IntToBool),
+                IsAutogranted = x.Get<bool>("allow_autogrant", JsonType.IntToBool),
                 Type = x.Get<string>("badge_type"),
                 ReviewStatus = x.Get<string>("review_status"),
                 Url = x.Get<string>("image_url"),
                 Creator = new Creator {
-                    Id = x.Get<string>("creator_id", JsonGetOptions.IntToString),
+                    Id = x.Get<string>("creator_id", JsonType.IntToStr),
                     Index = x.Get<int>("creator_badge_index")
                 },
                 Flag = new Flag {
@@ -173,5 +198,59 @@ public sealed class UserHandler {
         }
 
         return updated;
+    }
+
+    public async Task SaveUserAsync(int id) {
+        using var requestMessage = new HttpRequestMessage {
+            RequestUri = new Uri(Id(Endpoints.AVATAR_CARD, id)),
+            Method = HttpMethod.Get
+        };
+
+        using var responseMessage = await _httpClient.SendAsync(requestMessage);
+        if (!responseMessage.IsSuccessStatusCode) {
+            _logger.LogError("{ReasonPhrase}", responseMessage.ReasonPhrase);
+            return;
+        }
+
+        using var content = responseMessage.Content;
+        await using var stream = await content.ReadAsStreamAsync();
+        await JsonToUserAsync(stream);
+    }
+
+    public async Task<int> GetIdAsync(string username) {
+        using var data = new StringContent(
+            @$"
+            <methodCall>
+            <methodName>gateway.getUserIdForAvatarName</methodName>
+            <params>
+                <param>
+                    <value>
+                        <string>{username}</string>
+                    </value>
+                </param>
+            </params>s
+            </methodCall>",
+            Encoding.UTF8, "application/xml");
+
+        using var requestMessage = new HttpRequestMessage {
+            RequestUri = new Uri(Endpoints.GATEWAY_PHP),
+            Content = data,
+            Method = HttpMethod.Post
+        };
+
+        using var responseMessage = await _httpClient.SendAsync(requestMessage);
+        if (!responseMessage.IsSuccessStatusCode) {
+            _logger.LogError("{ReasonPhrase}", responseMessage.ReasonPhrase);
+            return default;
+        }
+
+        using var content = responseMessage.Content;
+        ReadOnlyMemory<byte> byteData = await content.ReadAsByteArrayAsync();
+        var slice = byteData[106..byteData.Span.IndexOf(EndSegment.Span)];
+        return int.Parse(Encoding.UTF8.GetString(slice.Span));
+    }
+
+    private static string Id(string str, int id) {
+        return str.Replace("{USER_ID}", $"{id}");
     }
 }
