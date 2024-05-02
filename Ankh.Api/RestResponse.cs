bf80a611acbb05d03735218e16bc4850c65308f0
Yucked <mail@dvlp.nill.sh>
2024-05-01 23:00:00 -0400

    RestResponse

---
+++ Ankh.Api/RestResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+using Ankh.Api.Models;
+
+namespace Ankh.Api;
+
+[JsonDerivedType(typeof(ProductModel))]
+public class RestResponse {
+    [JsonPropertyName("status")]
+    public string Status { get; init; }
+    
+    [JsonPropertyName("id")]
+    public string Id { get; init; }
+    
+}
+
+public record RestHttpModel {
+    
+}
