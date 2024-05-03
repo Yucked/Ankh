76ab417fba28b43f9985b9fe703d8bac2d548b7c
Yucked <mail@dvlp.nill.sh>
2024-05-03 18:21:04 -0400

    finally able to map api objects to models

---
+++ Ankh.Api/Converters/RestModelConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Ankh.Api.Models;
+
+namespace Ankh.Api.Converters;
+
+public class RestModelConverter : JsonConverter<IRestModel> {
+    public override IRestModel? Read(ref Utf8JsonReader reader, Type typeToConvert,
+                                     JsonSerializerOptions options) {
+        reader.Read();
+        var id = Encoding.UTF8.GetString(reader.ValueSpan);
+        
+        if (!JsonDocument.TryParseValue(ref reader, out var document)) {
+            return default;
+        }
+        
+        var result = id switch {
+            _ when id.Contains("product/product-") => document.RootElement
+                .GetProperty("data")
+                .Deserialize<ProductModel>()
+        };
+        
+        return result;
+    }
+    
+    public override void Write(Utf8JsonWriter writer, IRestModel value, JsonSerializerOptions options) {
+        throw new NotImplementedException();
+    }
+}
---
+++ Ankh.Api/Models/RestModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+using Ankh.Api.Converters;
+
+namespace Ankh.Api.Models;
+
+public interface IRestModel;
+
+public record RestModel {
+    [JsonPropertyName("status")]
+    public string Status { get; init; }
+    
+    [JsonPropertyName("id")]
+    public string Id { get; init; }
+    
+    [JsonPropertyName("denormalized"), JsonConverter(typeof(RestModelConverter))]
+    public IRestModel Data { get; set; }
+    
+    public IDictionary<string, string> Relations { get; set; }
+}
