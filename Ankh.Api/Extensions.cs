84607bba5d277c58411e18e583f105eb0719bdfd
Yucked <mail@dvlp.nill.sh>
2024-05-07 23:25:03 -0400

    turned repetitive code to ext

---
+++ Ankh.Api/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Json;
+using Ankh.Api.Models;
+
+namespace Ankh.Api;
+
+public static class Extensions {
+    public static async ValueTask<T> GetRestModelAsync<T>(this HttpClient httpClient, string requestUrl)
+        where T : IRestModel {
+        using var responseMessage = await httpClient.GetAsync(requestUrl);
+        if (!responseMessage.IsSuccessStatusCode) {
+            throw new Exception($"Failed to fetch {requestUrl} because of {responseMessage.ReasonPhrase}");
+        }
+        
+        var restModel = await responseMessage.Content.ReadFromJsonAsync<RestModel>();
+        if (restModel!.Status != "success") {
+            throw new Exception($"API returned following status: {restModel.Status}");
+        }
+        
+        return (T)restModel.Data;
+    }
+}
--- Ankh.Api/Handlers/UserHandler.cs
+++ Ankh.Api/Handlers/UserHandler.cs
@@ -1,5 +1,4 @@
-﻿using System.Net.Http.Json;
-using Ankh.Api.Models;
+﻿using Ankh.Api.Models;
 using Microsoft.Extensions.Logging;
 
 namespace Ankh.Api.Handlers;
@@ -7,23 +6,17 @@ namespace Ankh.Api.Handlers;
 public sealed class UserHandler(
     ILogger<UserHandler> logger,
     HttpClient httpClient) {
-    public async ValueTask<UserModel> GetUserByIdAsync(long userId) {
+    public ValueTask<UserModel> GetUserByIdAsync(long userId) {
         if (userId <= 0) {
             throw new ArgumentException("Can't be less than or equal to 0.", nameof(userId));
         }
         
-        var apiUrl = $"https://api.imvu.com/user/user-{userId}";
-        using var responseMessage = await httpClient.GetAsync(apiUrl);
-        if (!responseMessage.IsSuccessStatusCode) {
-            logger.LogError("Failed product fetch {}", userId);
-            throw new Exception("");
+        try {
+            return httpClient.GetRestModelAsync<UserModel>($"https://api.imvu.com/user/user-{userId}");
         }
-        
-        var restModel = await responseMessage.Content.ReadFromJsonAsync<RestModel>();
-        if (restModel!.Status != "success") {
-            throw new Exception("");
+        catch (Exception exception) {
+            logger.LogError(exception, "Something went wrong.");
+            throw;
         }
-        
-        return (UserModel)restModel.Data;
     }
 }
