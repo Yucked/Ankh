4a904f840d0ccccd001634e8e622c932f3d4de28
Yucked <mail@dvlp.nill.sh>
2024-05-07 22:48:00 -0400

    Get user by ID

---
+++ Ankh.Api/Handlers/UserHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http.Json;
+using Ankh.Api.Models;
+using Microsoft.Extensions.Logging;
+
+namespace Ankh.Api.Handlers;
+
+public sealed class UserHandler(
+    ILogger<UserHandler> logger,
+    HttpClient httpClient) {
+    public async ValueTask<UserModel> GetUserByIdAsync(long userId) {
+        if (userId <= 0) {
+            throw new ArgumentException("Can't be less than or equal to 0.", nameof(userId));
+        }
+        
+        var apiUrl = $"https://api.imvu.com/user/user-{userId}";
+        using var responseMessage = await httpClient.GetAsync(apiUrl);
+        if (!responseMessage.IsSuccessStatusCode) {
+            logger.LogError("Failed product fetch {}", userId);
+            throw new Exception("");
+        }
+        
+        var restModel = await responseMessage.Content.ReadFromJsonAsync<RestModel>();
+        if (restModel!.Status != "success") {
+            throw new Exception("");
+        }
+        
+        return (UserModel)restModel.Data;
+    }
+}
