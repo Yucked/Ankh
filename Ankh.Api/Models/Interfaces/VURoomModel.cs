cd7da729e7a63db3de150284653669148b1c0151
Yucked <mail@dvlp.nill.sh>
2024-05-09 22:11:38 -0400

    room properties common between rest/php models?

---
+++ Ankh.Api/Models/Interfaces/VURoomModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Ankh.Api.Models.Interfaces;
+
+/// <summary>
+/// Can be used for public rooms
+/// </summary>
+public record VURoomModel {
+    [JsonPropertyName("room_instance_id")]
+    public string RoomId { get; init; }
+    
+    [JsonPropertyName("name")]
+    public string Name { get; init; }
+    
+    [JsonPropertyName("is_ap")]
+    public bool IsAP { get; init; }
+    
+    [JsonPropertyName("is_vip")]
+    public bool IsVIP { get; init; }
+}
