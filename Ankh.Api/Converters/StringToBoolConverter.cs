35fffefb909b2d7fada2cd08a4f46d9c7e028ca4
Yucked <mail@dvlp.nill.sh>
2024-05-09 21:30:21 -0400

    str to bool conv

---
+++ Ankh.Api/Converters/StringToBoolConverter.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Ankh.Api.Converters;
+
+public class StringToBoolConverter : JsonConverter<bool> {
+    public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
+        var str = reader.GetString();
+        return !string.IsNullOrWhiteSpace(str) && bool.Parse(str);
+    }
+    
+    public override void Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options) {
+        throw new NotImplementedException();
+    }
+}
