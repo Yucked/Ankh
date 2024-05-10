5dbe5f826e105b53b87abd500649206e004a79b1
Yucked <mail@dvlp.nill.sh>
2024-05-09 22:11:52 -0400

    common props between rest/php model, needs more work

---
+++ Ankh.Api/Models/Interfaces/VUUserModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+using Ankh.Api.Attributes;
+
+namespace Ankh.Api.Models.Interfaces;
+
+public class VUUser {
+    [JsonPropertyNames("legacy_cid", "cid")]
+    public ulong UserId { get; init; }
+    
+    [JsonPropertyNames("username", "avname")]
+    public string Username { get; init; }
+    
+    [JsonPropertyNames("avatar_image", "avpic_url")]
+    public string AvatarUrl { get; init; }
+    
+    [JsonPropertyName("registered")]
+    public DateTimeOffset RegisteredOn { get; init; }
+}
