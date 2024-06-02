c605f943afe6ae72b3c6dc049929cd540e5d38b9
Yucked <mail@dvlp.nill.sh>
2024-06-02 14:01:32 -0400

    removed JsonPropertyNames.cs

--- Ankh.Api/Attributes/JsonPropertyNames.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Ankh.Api.Attributes;
-
-[AttributeUsage(validOn: AttributeTargets.Property | AttributeTargets.Field)]
-public sealed class JsonPropertyNames(params string[] names) : Attribute {
-    public string[] Names { get; } = names;
-}
