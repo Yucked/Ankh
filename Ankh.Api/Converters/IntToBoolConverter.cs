f98ed89d2f06b0342ee3910ba0082315fb32b2e9
Yucked <mail@dvlp.nill.sh>
2024-06-02 17:17:59 -0400

    fixed parsing for string -> int -> bool

--- Ankh.Api/Converters/IntToBoolConverter.cs
+++ Ankh.Api/Converters/IntToBoolConverter.cs
@@ -6,13 +6,17 @@ namespace Ankh.Api.Converters;
 public sealed class IntToBoolConverter : JsonConverter<bool> {
     public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
         return reader.TokenType switch {
-            JsonTokenType.True   => true,
-            JsonTokenType.False  => false,
-            JsonTokenType.String => bool.TryParse(reader.GetString(), out var b) ? b : throw new JsonException(),
+            JsonTokenType.True  => true,
+            JsonTokenType.False => false,
+            JsonTokenType.String => int.TryParse(reader.GetString(), out var i)
+                ? Convert.ToBoolean(i)
+                : bool.TryParse(reader.GetString(), out var b)
+                    ? b
+                    : throw new JsonException($"Unable to convert from string to bool"),
             JsonTokenType.Number => reader.TryGetInt64(out var l)
                 ? Convert.ToBoolean(l)
                 : reader.TryGetDouble(out var d) && Convert.ToBoolean(d),
-            _ => throw new JsonException(),
+            _ => throw new JsonException("Unable to convert from int to bool "),
         };
     }
     
