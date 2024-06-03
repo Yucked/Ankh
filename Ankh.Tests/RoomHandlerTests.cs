90e7d7870b246dc8bed8f3c98787c47039e2fa11
Yucked <mail@dvlp.nill.sh>
2024-06-03 18:08:11 -0400

    working on roomhandler tests

--- Ankh.Tests/RoomHandlerTests.cs
+++ Ankh.Tests/RoomHandlerTests.cs
@@ -4,7 +4,19 @@ namespace Ankh.Tests;
 public sealed class RoomHandlerTests {
     [DataTestMethod]
     [DataRow()]
-    public async Task Test_GetRoomByIdAsync(int roomId) {
-        Globals.RoomHandler.GetRoomByIdAsync(roomId);
+    public async Task Test_GetRoomByIdAsync(int userId, int roomId) {
+        //Globals.RoomHandler.GetRoomByIdAsync(userId, roomId);
+    }
+    
+    [DataTestMethod]
+    [DataRow("Foo", "Bar", "Query")]
+    public async Task Test_SearchRoomsAsync(string u, string p, string kw) {
+        var userSauce = await Globals.UserHandler.LoginAsync(u, p);
+        Assert.IsNotNull(userSauce);
+        Assert.IsNotNull(userSauce.Auth);
+        
+        await Globals.RoomHandler.SearchRoomsAsync(userSauce, q => {
+            q.Keywords = kw;
+        });
     }
 }
