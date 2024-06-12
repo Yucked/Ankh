3ac6cf3d7095d30f3b851c85e055bed86bf3fb4b
Yucked <mail@dvlp.nill.sh>
2024-06-11 21:40:50 -0400

    ProductSearchQuery.cs & RoomSearchQuery.cs

---
+++ Ankh/Models/Queries/ProductSearchQuery.cs
@@ -0,0 +1,72 @@
+﻿namespace Ankh.Models.Queries;
+
+/// <summary>
+/// https://api.imvu.com/product?
+/// product_rating=0
+/// filter_text=wig
+/// name_filter=wig
+/// partial_avatar_name=wig
+/// keywords=wig
+/// cat=40%2C41
+/// gender_restriction=male_compatible
+/// no_cat=316%2C324
+/// vcoin_price_min=0
+/// price_min=1
+/// include_histogram=true
+/// </summary>
+public sealed record ProductSearchQuery {
+    /// <summary>
+    /// 
+    /// </summary>
+    public int? ProductRating { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string FilterText { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string FilterName { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string PartialAvatarName { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Keywords { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public int[]? IncludedCategories { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public char? Gender { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public int[]? ExlcudedCategories { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public int? MinimumVCoinPrice { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public int MinimumPrice { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool IncludeHistogram { get; set; }
+}
---
+++ Ankh/Models/Queries/RoomSearchQuery.cs
@@ -0,0 +1,45 @@
+﻿using Ankh.Models.Enums;
+
+namespace Ankh.Models.Queries;
+
+public sealed record RoomSearchQuery {
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Keywords { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Language { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public string Username { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public int? MinOccupants { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public int? MaxOccupants { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool? HasPlusProducts { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool? RequiresAccessPass { get; set; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public ContentRating? Rating { get; set; }
+}
