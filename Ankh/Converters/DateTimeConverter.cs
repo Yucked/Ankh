9c967c5b54e0cd53eb5079a5a4b50dd5ca21921d
Yucked <mail@dvlp.nill.sh>
2024-06-14 15:20:51 -0400

    json can't handle a single string to datetime conversion

---
+++ Ankh/Converters/DateTimeConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Ankh.Converters;
+
+public sealed class DateTimeConverter : JsonConverter<DateTime> {
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
+        return DateTime.Parse(reader.GetString()!);
+    }
+    
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options) {
+        writer.WriteStringValue(value);
+    }
+}
