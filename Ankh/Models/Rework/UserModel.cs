97c3ddd0bd8431f45f3c908c6f6ed87488090d8b
Yucked <mail@dvlp.nill.sh>
2024-06-14 17:32:52 -0400

    removed saucery and moved props to UserModel

---
+++ Ankh/Models/Rework/UserModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Ankh.Models.Rework;
+
+public record UserModelEssential {
+    public int UserId { get; init; }
+    public string Username { get; init; }
+}
+
+public record UserLogin : UserModelEssential {
+    public string Password { get; init; }
+    public string Sauce { get; init; }
+    public string SessionId { get; init; }
+}
+
+public record UserModelMinimal : UserModelEssential { }
+
+public record UserModelCommon : UserModelMinimal {
+    public string ProfileImage { get; init; }
+    
+    [JsonPropertyName("registered")]
+    public DateTimeOffset RegisteredOn { get; init; }
+    
+    [JsonPropertyName("gender")]
+    public string Gender { get; init; }
+    
+    [JsonPropertyName("age")]
+    public int Age { get; init; }
+}
+
+public record UserModel { }
