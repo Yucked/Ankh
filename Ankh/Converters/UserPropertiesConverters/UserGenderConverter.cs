19d5b6e2e938bdff1b43a64bd44d6267a478884d
Yucked <mail@dvlp.nill.sh>
2024-06-25 01:01:29 -0400

    gender and interests converter

---
+++ Ankh/Converters/UserPropertiesConverters/UserGenderConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Ankh.Converters.UserPropertiesConverters;
+
+internal sealed class UserGenderConverter : JsonConverter<string> {
+    public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
+        var str = reader.GetString();
+        return str switch {
+            ""   => string.Empty,
+            null => string.Empty,
+            "m"  => "Male",
+            "f"  => "Female",
+            _    => str
+        };
+    }
+    
+    public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options) {
+        writer.WriteStringValue(value);
+    }
+}
---
+++ Ankh/Converters/UserPropertiesConverters/UserInterestConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Ankh.Converters.UserPropertiesConverters;
+
+internal sealed class UserInterestConverter : JsonConverter<string> {
+    public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
+        return reader.TokenType == JsonTokenType.StartObject
+            ? JsonDocument.Parse(reader.ValueSequence).RootElement
+                .GetProperty("full_text_string")
+                .GetProperty("raw_tag")
+                .GetString()!
+            : reader.GetString()!;
+    }
+    
+    public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options) {
+        writer.WriteStringValue(value);
+    }
+}
